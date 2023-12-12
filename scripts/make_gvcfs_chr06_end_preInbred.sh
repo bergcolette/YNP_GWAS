e30f02931774aad1f172487c772719edacc8138e
Colette Berg <bergcolette@gmail.com>
2023-12-12 23:13:04 +0000

    combining GVCFs, calling SNPs at the end of chr06

---
+++ scripts/make_gvcfs_chr06_end_preInbred.sh
@@ -0,0 +1,11 @@
+mainDir="/scratch/colette/demux_novogene_lane_2023/bams/preInbredLines"
+genome="/home/colette_berg/resources/reference_genomes/AHQT/Mimulus_guttatus_var_AHQT1.mainGenome.fa"
+
+for i in ${mainDir}/*end.bam;
+do java -jar ~/resources/packages/gatk-4.2.6.1/gatk-package-4.2.6.1-local.jar HaplotypeCaller \
+-R ${genome} \
+-I $i \
+-O ${i:0:-4}.g.vcf.gz \
+--heterozygosity 0.05 \
+--ERC GVCF;
+done
