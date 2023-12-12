ae34775433f72bdccd3f2950d9dbab6c8556fcf4
Colette Berg <bergcolette@gmail.com>
2023-12-12 00:07:35 +0000

    redoing remove duplicates, wrote vcf-calling scripts

--- scripts/rmdup.sh
+++ scripts/rmdup.sh
@@ -1,5 +1,7 @@
-for i in *bam; do java -jar /home/thom_nelson/opt/picard.jar MarkDuplicates \
-    INPUT=${i} OUTPUT=${i}.rmdup.bam \
-    METRICS_FILE=${i}.rmdup_metrics_fix \
+mainDir="/scratch/colette/demux_novogene_lane_2023/bams"
+
+for i in ${mainDir}/*bam; do java -jar /home/thom_nelson/opt/picard.jar MarkDuplicates \
+    INPUT=${i} OUTPUT=${i:0:-4}.rmdup.bam \
+    METRICS_FILE=${i:0:-4}.rmdup_metrics_fix \
     VALIDATION_STRINGENCY=LENIENT REMOVE_DUPLICATES=TRUE;
 done
