5a35e2801edab0420d3a470f5a15660cc11ea5a1
Colette Berg <bergcolette@gmail.com>
2023-12-29 22:40:40 +0000

    lower coverage after chr08 for some reason? also converting stringent vcf to plink

--- scripts/filter_SNPs_wholeGenome_then.sh
+++ scripts/filter_SNPs_wholeGenome_then.sh
@@ -1,5 +1,8 @@
 vcftools --gzvcf YNP_WGS_allCombined_withYGBR_9.1.vcf.gz \
 --minDP 6 \
+--thin 150 \
+--max-missing .8 \
+--minQ 29 \
 --min-alleles 2 \
---out YNP_WholeGenome \
+--out YNP_WholeGenome_stringent \
 --recode
