536005434a78f7a960a1785a316ac5ef842e9557
Colette Berg <coletteberg@Colettes-MacBook-Air.local>
2024-01-01 16:49:30 -0700

    R files for analyzing the vcf

---
+++ analyzing_inbred_lines.R
@@ -0,0 +1,9 @@
+# this is R scripts for analysing inbred line genomic data 
+
+# loading my libraryes
+library(hierfstat)
+library(adegenet)
+library(vcfR)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
