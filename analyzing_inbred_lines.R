704f941984253201176af5b0ee9ee7494592e124
Colette Berg <coletteberg@Colettes-MacBook-Air.local>
2024-01-01 18:31:17 -0700

    making a PCA of the genotype data

--- analyzing_inbred_lines.R
+++ analyzing_inbred_lines.R
@@ -7,3 +7,28 @@ library(vcfR)
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
+
+
+
+
+# read in vcf / convert to genind format 
+YNP_inbredLines <- read.vcfR("YNP_WholeGenome_stringent.recode.vcf")
+
+# convert to genind 
+YNP_inbredLines_genind <- vcfR2genind(YNP_inbredLines)
+
+# remove some of the individuals 
+
+# nas aren't allowed (annoying, so replacing)
+
+YNP_inbredLines_genind <- tab(YNP_inbredLines_genind,
+                              NA.method="mean")
+
+# run a PCA to see if it fits expectations / matches geography 
+pca.YNP <- dudi.pca(YNP_inbredLines_genind,
+                    center=TRUE)
+
+# add names & plot 
+s.label(pca.YNP$li)
+
+
