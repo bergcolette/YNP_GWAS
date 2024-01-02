27fa67d24b05b4a7829204b3d16e52c4ed24bfa6
Colette Berg <coletteberg@Colettes-MacBook-Air.local>
2024-01-01 19:18:59 -0700

    making PCAs, adding population info

--- analyzing_inbred_lines.R
+++ analyzing_inbred_lines.R
@@ -9,26 +9,46 @@ library(tidyverse)
 library(ggplot2)
 
 
-
-
 # read in vcf / convert to genind format 
-YNP_inbredLines <- read.vcfR("YNP_WholeGenome_stringent.recode.vcf")
+YNP_inbredLines <- read.vcfR("YNP_WholeGenome_stringent_chr01.recode.vcf")
 
 # convert to genind 
 YNP_inbredLines_genind <- vcfR2genind(YNP_inbredLines)
 
+
+# first changing the names because the vcf names are way too long 
+indv_info <- read.csv("YNP_names.csv")
+indNames(YNP_inbredLines_genind) <- indv_info$names
+
+# adding in population 
+pop(YNP_inbredLines_genind) <- indv_info$clust
+
 # remove some of the individuals 
+removeInd <- c("YLRC_2.3", "LMC3-1", "LMC2-3",
+               "RCT433", "PVS127", "PVS300", "PVS329",
+               "UGB11-3", "HLB4-2", 
+               "AHQT_1.2", "AHQNT",
+               "YVTC_9.3")
+
+data <- YNP_inbredLines_genind[!row.names(YNP_inbredLines_genind@tab) %in% removeInd]
+#data
 
 # nas aren't allowed (annoying, so replacing)
 
-YNP_inbredLines_genind <- tab(YNP_inbredLines_genind,
+YNP_for_PCA <- tab(data,
                               NA.method="mean")
 
 # run a PCA to see if it fits expectations / matches geography 
-pca.YNP <- dudi.pca(YNP_inbredLines_genind,
+pca.YNP <- dudi.pca(YNP_for_PCA,
                     center=TRUE)
 
 # add names & plot 
 s.label(pca.YNP$li)
+
+# add a plot with the populations 
+s.class(pca.YNP$li, fac=pop(data),
+        col=transp(funky(15),.6),
+        axesel=FALSE, cstar=0, cpoint=3)
+
 
 
