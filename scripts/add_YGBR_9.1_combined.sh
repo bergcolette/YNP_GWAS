3f58d7d0ef2009a00872e8e1fc2e5c4e3d9655fc
Colette Berg <bergcolette@gmail.com>
2024-01-03 18:36:12 +0000

    think I solved the chr_08 issue, now making vcf with the right individuals

--- scripts/add_YGBR_9.1_combined.sh
+++ scripts/add_YGBR_9.1_combined.sh
@@ -4,5 +4,6 @@ genome="/home/colette_berg/resources/reference_genomes/AHQT/Mimulus_guttatus_var
 java -jar ~/resources/packages/gatk-4.2.6.1/gatk-package-4.2.6.1-local.jar  CombineGVCFs \
    -R ${genome} \
    --variant /scratch/colette/demux_novogene_lane_2023/YNP_GWAS/scripts/YNP_WGS_allCombined.g.vcf.gz \
-	--variant ${mainDir}/YGBR_9.1_WGS_AHQTv1.g.vcf.gz \
--O YNP_WGS_allCombined_YGBR_:wq9.1_included.g.vcf.gz
+	--variant ${mainDir}/YWTB_1.2_WGS_AHQTv1.g.vcf.gz \
+	--variant ${mainDir}/YWTB_7.2_WGS_AHQTv1.g.vcf.gz \
+-O YNP_WGS_allCombined_two_more_added.g.vcf.gz
