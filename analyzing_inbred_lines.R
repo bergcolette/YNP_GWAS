7b45d9258e06db1a0883b27246c3101b42eb850e
Colette Berg <coletteberg@Colettes-Air.lan>
2024-01-10 17:38:05 -0700

    analyzing inbred lines LD patterns, & comparing different relatedness metrics

--- analyzing_inbred_lines.R
+++ analyzing_inbred_lines.R
@@ -8,31 +8,31 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 
+setwd("~/Documents/UMontana/Research/YNP/YNP_GWAS/")
 
 # read in vcf / convert to genind format 
-YNP_inbredLines <- read.vcfR("YNP_WholeGenome_stringent_chr01.recode.vcf")
+YNP_inbredLines <- read.vcfR("YNP_thinned_biallelic.recode.vcf")
 
 # convert to genind 
 YNP_inbredLines_genind <- vcfR2genind(YNP_inbredLines)
 
 
 # first changing the names because the vcf names are way too long 
-indv_info <- read.csv("YNP_names.csv")
-indNames(YNP_inbredLines_genind) <- indv_info$names
+#indv_info <- read.csv("YNP_names.csv")
+
+#rem <- filter(indv_info, keep == 0)
+#indNames(YNP_inbredLines_genind) <- indv_info$names
 
 # adding in population 
-pop(YNP_inbredLines_genind) <- indv_info$clust
+#pop(YNP_inbredLines_genind) <- indv_info$clust
 
 # remove some of the individuals 
-removeInd <- c("YLRC_2.3", "LMC3-1", "LMC2-3",
-               "RCT433", "PVS127", "PVS300", "PVS329",
-               "UGB11-3", "HLB4-2", 
-               "AHQT_1.2", "AHQNT",
-               "YVTC_9.3")
+#removeInd <- rem$names
 
-data <- YNP_inbredLines_genind[!row.names(YNP_inbredLines_genind@tab) %in% removeInd]
+#data <- YNP_inbredLines_genind[!row.names(YNP_inbredLines_genind@tab) %in% removeInd]
 #data
-
+#rem
+data <- YNP_inbredLines_genind
 # nas aren't allowed (annoying, so replacing)
 
 YNP_for_PCA <- tab(data,
@@ -45,6 +45,17 @@ pca.YNP <- dudi.pca(YNP_for_PCA,
 # add names & plot 
 s.label(pca.YNP$li)
 
+dat <- pca.YNP$li
+write.csv(dat, "PC_axes.csv")
+
+ggplot(dat, aes(x=Axis1, y=Axis2)) + geom_point() 
+
++ 
+  geom_label(label=rownames(dat), 
+nudge_x = 0.25, nudge_y = 0.25)
+
+#rownames(dat)
+
 # add a plot with the populations 
 s.class(pca.YNP$li, fac=pop(data),
         col=transp(funky(15),.6),
@@ -53,13 +64,57 @@ s.class(pca.YNP$li, fac=pop(data),
 
 # making a relatedness heat map 
 
-rel <- read.csv("YNP_stringent_chr01_filtIndv.relatedness", sep="\t")
+rel <- read.csv("~/Documents/UMontana/Research/YNP/YNP_GWAS/pc_relate.csv")
 
-ggplot(rel, aes(x=INDV2, y=INDV1,
-                fill=RELATEDNESS_AJK)) + geom_tile() +
-  scale_fill_gradient2(low="blue", high="red")
+ggplot(rel, aes(x=i.s, y=j.s,
+                fill=kin)) + geom_tile() +
+  scale_fill_gradient2(low="blue", high="red") +
+  theme(axis.text.x = element_text(angle=90),
+        axis.text = element_text(size=8))
 
 
-heatmap(rel)
 
-rel$INDV1
+# calculating LD 
+
+LD_chr05 <- read.csv("YNP_chr05_genic_thin.geno.ld", sep="\t")
+LD_chr04 <- read.csv("YNP_chr04_genic_thin.geno.ld", sep="\t")
+
+LD_chr07 <- read.csv("YNP_chr07_genic_thin.geno.ld", sep="\t")
+LD_chr08 <- read.csv("YNP_chr08_genic_thin.geno.ld", sep="\t")
+
+
+LD_chr05 <- mutate(LD_chr05, dist = POS2 - POS1)
+#lheatmap(rel)
+
+ggplot(filt_LD_chr05, aes(x=dist, y=R.2)) + geom_point() + geom_smooth()
+
+filt_LD_chr05 <- filter(LD_chr05, R.2 != "NaN")
+
+ggplot(filt_LD_chr06, aes(x=R.2)) + geom_histogram()
+
+
+
+ggplot(filt_LD_chr05, aes(x=as.factor(POS1), y=as.factor(POS2),
+                fill=R.2, col=R.2)) + geom_tile()  +
+  scale_fill_gradient2(low="blue", high="red") +
+  scale_color_gradient2(low="blue", high="red") +
+  theme(axis.text.x = element_text(angle=90),
+        axis.text = element_text(size=8))
+
+dat <- aggregate(filt_LD_chr05$POS1, list(filt_LD_chr05$R.2),
+          mean)
+
+ggplot(filter(dat, Group.1 > .1), aes(x=x, 
+                y=Group.1)) + geom_point() + 
+  geom_smooth()
+#+
+#  theme(axis.text.x = element_text(angle=90),
+#        axis.text = element_text(size=8))
+
+
+
+
+
+
+
+
