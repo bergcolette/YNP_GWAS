a1aba33476eabc6dcdd79f7e0ffe2a626f4b4dda
Colette Berg <bergcolette@gmail.com>
2024-01-16 18:47:52 +0000

    pi calcs and Tajima's D with a vcf that includes invariant sites, but is filtered by depth & quality

--- scripts/tajD_and_pi.sh.sh
+++ scripts/tajD_and_pi.sh.sh
@@ -2,19 +2,19 @@ dataDir="/scratch/colette/demux_novogene_lane_2023/YNP_GWAS/data"
 
 vcftools --vcf YNP_chr05_group1.recode.vcf \
 --out ${dataDir}/YNP_chr05_group1 \
---TajimaD 10000
+--TajimaD 100
 
-#vcftools --vcf YNP_chr05_group2.recode.vcf \
-#--out ${dataDir}/YNP_chr05_group2 \
-#--TajimaD 10000
+vcftools --vcf YNP_chr05_group2.recode.vcf \
+--out ${dataDir}/YNP_chr05_group2 \
+--TajimaD 100
 
 vcftools --vcf YNP_chr05_group1.recode.vcf \
 --out ${dataDir}/YNP_chr05_group1 \
---window-pi 1000 
+--window-pi 100 
 
-#vcftools --vcf YNP_chr05_group2.recode.vcf \
-#--out ${dataDir}/YNP_chr05_group2 \
-#--window-pi 1000 
+vcftools --vcf YNP_chr05_group2.recode.vcf \
+--out ${dataDir}/YNP_chr05_group2 \
+--window-pi 100
 
 
 
